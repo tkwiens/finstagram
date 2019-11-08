6aed061b20fe34c96dd0a5c994fe29a4725e0b31
tekarra.w <tekarra.w@gmail.com>
2019-11-07 21:55:55 -0500

    push for deploy

---
+++ app/models/finstagram_post.rb
@@ -0,0 +1,31 @@
+class FinstagramPost < ActiveRecord::Base
+
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  validates :photo_url, :user, presence: true
+
+  def humanized_time_ago
+    time_ago_in_seconds = Time.now - self.created_at
+    time_ago_in_minutes = time_ago_in_seconds / 60
+
+    if time_ago_in_minutes >= 120
+      "#{(time_ago_in_minutes / 60).to_i} hours ago"
+      
+    elsif time_ago_in_minutes > 60 and time_ago_in_minutes < 120
+           "1 hour ago"
+   else
+      "#{time_ago_in_minutes.to_i} minutes ago"
+    end
+  end
+  # New Stuff Start 
+  def like_count
+    self.likes.size
+  end
+
+  def comment_count
+    self.comments.size
+  end
+  # New Stuff End
+end
---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Like < ActiveRecord::Base
+  
+  belongs_to :user
+  belongs_to :finstagram_post
+  
+    validates_presence_of :user, :finstagram_post
+
+  
+end
